afc004d8e87969cddd60a47e7f35bebbba38604b
Alicia Fernandez <alicia.fernandez@bosonit.com>
2022-08-02 13:01:30 +0200

    end of section 5

--- src/app/dbz/services/dbz.service.ts
+++ src/app/dbz/services/dbz.service.ts
@@ -18,7 +18,9 @@ export class DbzService {
     return [...this._personajes];
   }
 
-  constructor() {
-    console.log('Servicio inicializado');
+  constructor() {}
+
+  agregarPersonaje(personaje: Personaje) {
+    this._personajes.push(personaje);
   }
 }
